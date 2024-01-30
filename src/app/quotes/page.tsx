2d1d721f7b525a243636a228a218c13c04850806
张磊 <zhanglei@cdgis.cc>
2024-01-30 14:54:09 +0800

    ss

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -1,13 +1,7 @@
-
-
-
 import React from "react";
 import { api } from "~/trpc/server";
 import localFont from "next/font/local";
 import SortTabs from "~/app/_components/sort-tabs";
-
-
-
 
 export const dynamic = "force-static";
 const fontSTKaiti = localFont({
@@ -21,32 +15,21 @@ export default async function page() {
   const allPost = await api.quotes.getList.query();
 
   return (
-    <div className={myFont.className}>
-      <SortTabs sort='updatedAt'/>
-      <main className=" m-auto flex h-screen max-w-screen-md flex-col space-y-4">
-        {allPost.map((item, index) => {
-          return (
-            <div
-              className="hover:bg-accent  hover:text-accent-foreground  border-border rounded-md border p-4 hover:shadow-md"
-              key={index}
-            >
-              <h1>{item.content}</h1>
-            </div>
-          );
-        })}
-      </main>
-    </div>
+    //   <SortTabs sort='updatedAt'/>
 
-    //  <div>
-    //   {
-    //     allPost.map((item,index)=>{
-    //       return (
-    //         <div key={index}>
-    //           <h1>{item.content}</h1>
-    //         </div>
-    //       )
-    //     })
-    //   }
-    //  </div>
+    <div
+      className={`grid grid-flow-row-dense grid-cols-3 gap-4 ${myFont.className}`}
+    >
+      {allPost.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="hover:bg-accent hover:text-accent-foreground  border-border  col-span-1 flex items-center justify-center rounded-md border p-4 hover:shadow-md"
+          >
+            <h1>{item.content}</h1>
+          </div>
+        );
+      })}
+    </div>
   );
 }
